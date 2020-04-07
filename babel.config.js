d78abe8d12342c1cb4e54bbe8a0ddd8f94fbf1ab
Petrov Dmitry <dima.krutoo@yandex.ru>
2020-04-07 17:44:37 +0500

    configs added

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,24 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-    '@babel/preset-react',
-  ],
+  sourceMaps: true,
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+        '@babel/preset-react',
+      ],
+    },
+    build: {
+      presets: [
+        '@babel/preset-modules',
+        '@babel/preset-react',
+      ],
+    },
+  },
 };
